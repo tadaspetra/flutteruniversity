843ef50f0dab11ef7cfbadf14b3444c67aa88bfe
Tadas Petra <connectwithamateur@gmail.com>
2020-12-22 16:38:17 -0600

    updates after recording

--- 1_Introduction To Programming/2_BuildingBlocks/buildingblocks.dart
+++ 1_Introduction To Programming/2_BuildingBlocks/buildingblocks.dart
@@ -4,33 +4,33 @@ void main() {
   print('FLUTTER UNIVERSITY');
   print('------------------');
 
-  //messy variables, but code works
-  int asdfsadf = 20;
-  int wqklejrq = 5;
-  int bafawera = asdfsadf + wqklejrq;
-  print(bafawera);
+  // //bad variable names
+  // int asfdsadfas = 20;
+  // int xzcveqherl = 5;
+  // int gbjlasdlas = asfdsadfas + xzcveqherl;
+  // print(gbjlasdlas);
 
-  //better variable name, and still works
-  int _price = 20;
-  int _tax = 5;
-  int _total = _price + _tax;
-  print(_total);
+  // //better variable names
+  // int _price = 20;
+  // int _tax = 5;
+  // int _total = _price + _tax;
+  // print(_total);
 
-  //converting data types
-  var _x = "1";
-  // var _y = _x + 1;
-  // print("y:" + _y);
-  print(_x.runtimeType);
-  var _y = int.parse(_x) + 1;
-  print("y:" + _y.toString());
+  // //converting datatypes
+  // var _x = '1';
+  // print(_x.runtimeType);
+  // var _y = int.parse(_x) + 1;
+  // print("y: $_y");
+  // print("y: " + _y.toString());
 
-  //user input
-  print("What is your name?");
-  String _name = stdin.readLineSync();
-  print("Hello, $_name");
+  // //user input
+  // print("What is your name?");
+  // String _name = stdin.readLineSync();
+  // print("Hello, $_name");
 
   //converting celcius to fahrenheit
-  print("What is the temperature in Celcius?");
+  //PEMDAS - Paranthesis, Exponents, Multiplication, Division, Addition, Subtraction
+  print("What is the temprature in Celcius?");
   double _celcius = double.parse(stdin.readLineSync());
   double _fahrenheit = _celcius * (9 / 5) + 32;
   print("Fahrenheit: $_fahrenheit");
